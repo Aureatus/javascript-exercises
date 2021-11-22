6a2357657743c31253cb98d313d21c4e8d7a9c74
ZanyAlex <alextknott@gmail.com>
2021-11-22 16:54:36 +0000

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let repeatedString = string;
+    for (let i = num-1; i>0,i--;) {
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
