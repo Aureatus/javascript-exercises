d51dae7939cd056e0e30c495692704f7cfdf47dd
ZanyAlex <alextknott@gmail.com>
2021-11-23 13:22:20 +0000

    Completed reverseString task with all tests passing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function(str) {
+// Split string into single letters inside an array, then use a for loop to reconstruct it back to front.
 
+const reverseString = function(str) {
+    const splitString = str.split("")
+    const reversedSplitString = splitString.reverse();
+    const joinedString = reversedSplitString.join("");
+    return joinedString
 };
 
 // Do not edit below this line
