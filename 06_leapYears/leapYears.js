291d50ac6a66e385d21f9c0a9b640f53964232b1
ZanyAlex <alextknott@gmail.com>
2021-11-23 15:15:52 +0000

    Completed exercise 6 with all tests completed successfuly

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    dividedBy4 = year / 4
+    dividedBy100 = year / 100
+    dividedBy400 = year / 400
+    divisionBy100Check = Number.isInteger(dividedBy100)
+    console.log(Number.isInteger(dividedBy400))
+    if (Number.isInteger(dividedBy4)) {
+        if ((divisionBy100Check) && (Number.isInteger(dividedBy400))) {
+            return true
+        }
+        else if (divisionBy100Check) {
+            return false
+        }
+        return true
+    }
+    else return false
 };
 
 // Do not edit below this line
