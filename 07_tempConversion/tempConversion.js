f34090ffb705f19ebea5b8df9b8478a75fcf5ed1
ZanyAlex <alextknott@gmail.com>
2021-11-23 15:26:05 +0000

    Completed exercise 7 with all tests being passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(input) {
+  let num = input
+  let celsiusValue = (((num - 32)*5)/9)
+  celsiusValue = Math.round(celsiusValue * 10) / 10
+  return celsiusValue
 
 };
 
-const ctof = function() {
+const ctof = function(input) {
+  let num = input
+  let fahrenheitValue = (((num/5)*9) + 32)
+  fahrenheitValue = Math.round(fahrenheitValue * 10) / 10
+  return fahrenheitValue
 
 };
 
