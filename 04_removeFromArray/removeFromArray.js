d1091db7a9f8c6a146eb547e10f140355d7404aa
ZanyAlex <alextknott@gmail.com>
2021-11-23 14:37:21 +0000

    Finished exercise 04, fulfilling all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let temp = [];
+    for (let i = 1; i < arguments.length; i++) {
+        temp.push(arguments[i]);
+        arr = arguments[0].filter(function(value) {
+            return (value !== temp[i - 1]) ;
+        });
+    }
+    return arr;
 };
 
 // Do not edit below this line
