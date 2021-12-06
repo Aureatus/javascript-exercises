ec10fb73ccb7f585e5d596b9ecb8e0539613e402
Zesty Alex <alextknott@gmail.com>
2021-12-06 12:11:24 +0000

    Finished all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1+num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return (num1-num2)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const reducer = (previousValue, currentValue) => (previousValue + currentValue);
+  let arraySum = array.reduce(reducer, 0);
+  return arraySum 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const reducer = (previousValue, currentValue) => (previousValue * currentValue);
+  let arrayMultiplied = array.reduce(reducer);
+  return arrayMultiplied
 };
 
-const power = function() {
-	
-};
+const power = function(num1,num2) {
+  return (num1**num2)
+}
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0 || n == 1){
+    return 1;
+  }else{
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
