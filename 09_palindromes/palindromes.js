744688fd5d28f554f409500fdb93b3611f446e4b
Zesty Alex <alextknott@gmail.com>
2021-12-06 15:27:03 +0000

    All palindrome tests complete successfully

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
+    str = str.toLowerCase()
+    str = str.replaceAll(" " ,"")
+    console.log(str)
+    let splitStr = str.split("")
+    let reverseArray = splitStr.reverse()
+    let joinArray = reverseArray.join("")
 
-};
+    if (str === joinArray) {
+        return true
+    } else{
+        return false
+    }
 
+}
 // Do not edit below this line
 module.exports = palindromes;
