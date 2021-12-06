c3120c503b5731b71615e853d5b8f8aa03822e8a
Zesty Alex <alextknott@gmail.com>
2021-12-06 18:08:11 +0000

    Completes all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = []
+    let currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
+    for(let key in people) {
+        if (people[key].yearOfDeath === undefined) {
+            let age = currentYear - (people[key].yearOfBirth)
+            ages.push(age)
+        } 
+        else if (((currentYear) - (people[key].yearOfBirth) ) > 120) {
+            let index = people.indexOf((currentYear) - (people[key].yearOfBirth));
+            if (index > -1) {
+                people.splice(index, 1);
+            }
+        }
+        else {
+            let age = (people[key].yearOfDeath) - (people[key].yearOfBirth)
+            ages.push(age)
+        }
+    }
+    let oldestAge = Math.max.apply(null, ages);
+    function checkAge (ages) {
+        return ages === oldestAge
+    }
+    let oldestAgeIndex =ages.findIndex(checkAge)
+    return people[oldestAgeIndex]
 };
 
 // Do not edit below this line
