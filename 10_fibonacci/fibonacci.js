c0e88d916d023c076bb2bbf0209ffcc2a685ca0e
Zesty Alex <alextknott@gmail.com>
2021-12-06 15:50:49 +0000

    Passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS"
+    }
+    let a = 1
+    let b = 0
+    let c
+    for(let i = input; i > 1; i--) {
+        c = a
+        a = a+b;
+        b = c
+    }
+    return a
 };
 
 // Do not edit below this line
