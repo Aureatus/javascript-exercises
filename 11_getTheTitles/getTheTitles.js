0a40d9bf02c1dceee602bcb310ab44751f29271d
Zesty Alex <alextknott@gmail.com>
2021-12-06 16:24:19 +0000

    Passes the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+    let titles = []
+    for (let key in input) {
+        titles.push(input[key].title)
+    }
+    return titles
 };
 
 // Do not edit below this line
